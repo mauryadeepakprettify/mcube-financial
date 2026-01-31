61f56211cc069ca83dc06f410c4dc3ecdb851d7a
mauryadeepakprettify <mauryadeepak.prettify@gmail.com>
2026-01-31 18:46:17 +0530

    redux: setup

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import MainTemplate from "@/components/templates/MainTemplate";
 import "./globals.css";
 import localFont from "next/font/local";
+import ReduxProvider from "@/store/provider";
 
 const celeste = localFont({
   src: "../../public/fonts/CelesteOT.otf",
@@ -22,7 +23,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${celeste.variable} ${partner.variable}`}>
-        <MainTemplate>{children}</MainTemplate>
+        <ReduxProvider>
+          <MainTemplate>{children}</MainTemplate>
+        </ReduxProvider>
       </body>
     </html>
   );
---
+++ src/store/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import modalReducer from "./slices/modalSlice";
+
+const store = configureStore({
+  reducer: {
+    modal: modalReducer,
+  },
+});
+
+export default store;
